6e9715149881506ac5eb3707773ce4eda47c358f
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2010-03-02 21:59:31 +0000

    Version 0.23.2 epsilon. Other small details.

--- trunk/src/dialogs/DashInfo.java
+++ trunk/src/dialogs/DashInfo.java
@@ -2,7 +2,9 @@ package dialogs;
 
 import primitives.*;
 
-/** 
+/** This class contains some settings about the actual dashing style. It is used
+	in the automatic primitive characteristics dialog.
+	
 	@author Davide Bucci
 	@version 1.0 November 2009
 	
