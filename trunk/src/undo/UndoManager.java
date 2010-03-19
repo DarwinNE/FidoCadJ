a2f36d862c9ca092b35946f04275652375215c2d
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2010-03-19 21:16:37 +0000

    Added comments and licensing

--- trunk/src/undo/UndoManager.java
+++ trunk/src/undo/UndoManager.java
@@ -3,21 +3,60 @@ package undo;
 import java.util.*;
 
 
-/**	Implementation of a circular buffer of the given size.
 
+
+/**
+   	Implementation of a circular buffer of the given size.
+    This is tailored in particular for the undo/redo system.
+    The choice of the circular buffer is reasonable since one expects that 
+    in the classical undo systems the very old states are overwritten when
+    the maximum number of undo steps is reached.
+    
+    @author Davide Bucci
+    @version 1.0, January 2009
+ 
+
+
+<pre>
+	This file is part of FidoCadJ.
+
+    FidoCadJ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FidoCadJ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FidoCadJ.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright 2008-2009 by Davide Bucci
+</pre>
+
+@author Davide Bucci
 */
+
 public class UndoManager {
 	
 	private Vector undoBuffer;
 	private int pointer;
 	private boolean isRedoable;
 	
+	/** Creates a new undo buffer of the given size
+	
+	*/
 	public UndoManager (int size)
 	{
 		undoBuffer = new Vector(size);
 		undoReset();
 	}
 	
+	/** Removes all the elements from the circular buffer
+	
+	*/
 	public void undoReset()
 	{
 		undoBuffer.removeAllElements();
@@ -26,6 +65,9 @@ public class UndoManager {
 		isRedoable=false;
 	}
 	
+	/** Pushes a new undo state in the buffer
+		@argument state the state to be committed.
+	*/
 	public void undoPush(Object state)
 	{	
 		if(undoBuffer.size()==undoBuffer.capacity()) {
@@ -33,7 +75,6 @@ public class UndoManager {
 			--pointer;
 		}
 		undoBuffer.add(pointer++, state);
-//		System.out.println(undoBuffer);
 
 		isRedoable=false;
 		for(int i=pointer; i<undoBuffer.size();++i)
@@ -41,6 +82,9 @@ public class UndoManager {
 	}
 
 
+	/** Pops the last undo state from the buffer
+		@return the recovered state.
+	*/
 	public Object undoPop()
 		throws NoSuchElementException
 	{	
@@ -48,14 +92,15 @@ public class UndoManager {
 		if(pointer<1)
 			pointer=1;
 		Object o=undoBuffer.get(pointer-1);
-			
-		//undoBuffer.removeElementAt(undoBuffer.size()-1);
-//		System.out.println(undoBuffer);
 
 		isRedoable=true;
 		return o;
 		
 	}
+	
+	/** Redo the last undo state from the buffer
+		@return the recovered state.
+	*/
 	
 	public Object undoRedo()
 		throws NoSuchElementException
