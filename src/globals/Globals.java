2e9cbc6696a32ea27494e6ce1dc199ef1d26f63d
Davide Bucci <DarwinNE@users.noreply.github.com>
2010-12-19 21:16:35 +0000

    Restored the Edit Layer button in the Layer dialog.

--- src/globals/Globals.java
+++ src/globals/Globals.java
@@ -84,7 +84,7 @@ public class Globals
     
   								
     // Version. This is shown in the main window title bar
-    public static final String version = "0.23.5 beta";
+    public static final String version = "0.23.5 gamma";
     // Is it a beta version?
     public static final boolean isBeta = true;		
     
