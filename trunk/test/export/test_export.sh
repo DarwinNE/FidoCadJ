af8c118b014c9a6fa13cb0e4ac45d170ce09822a
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2011-05-24 00:27:28 +0000

    -n option now activates the headless mode.

--- trunk/test/export/test_export.sh
+++ trunk/test/export/test_export.sh
@@ -8,22 +8,22 @@ echo
 
 echo "Testing the export on bitmap formats"
 echo "PNG (Portable Network Graphic)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 png png/test_out.png original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 png png/test_out.png original/test_pattern.fcd
 echo "JPG (Joint Photographic Experts Group)" 
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 jpg jpg/test_out.jpg original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 jpg jpg/test_out.jpg original/test_pattern.fcd
 echo
 
 echo Testing the export on vector formats
 echo "SVG (Scalar Vector Graphic)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 svg svg/test_out.svg original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 svg svg/test_out.svg original/test_pattern.fcd
 echo "EPS (Encapsulated Postscript)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 eps eps/test_out.eps original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 eps eps/test_out.eps original/test_pattern.fcd
 echo "PDF (Portable Document Format)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 pdf pdf/test_out.pdf original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 pdf pdf/test_out.pdf original/test_pattern.fcd
 echo "SCR (script for CadSoft Eagle)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r2 scr scr/test_out.scr original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r2 scr scr/test_out.scr original/test_pattern.fcd
 echo "PGF (PGF/TikZ script for LaTeX)"
-java -Djava.awt.headless=true -jar ../../jar/fidocadj.jar -n -c r1.5 pgf pgf/test_out.pgf original/test_pattern.fcd
+java -jar ../../jar/fidocadj.jar -n -c r1.5 pgf pgf/test_out.pgf original/test_pattern.fcd
 cd pgf
 pdflatex test_out.tex
 rm test_out.aux
