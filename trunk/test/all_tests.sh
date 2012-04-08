ccf253a6d89d0213f0aaeb4029e3f4b7b1796795
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2012-04-08 22:26:03 +0000

    Some tweaks to the test suite.

--- trunk/test/all_tests.sh
+++ trunk/test/all_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo ""
-echo "   F i d o C a d J"
+echo -e "\033[1m   F i d o C a d J"
 echo ""
 echo "   Automatic test suite"
-echo "   by Davide Bucci 2011"
-echo ""
+echo "   by Davide Bucci 2011-2012"
+echo -e "\033[0m"
 echo ""
 
 cd export
