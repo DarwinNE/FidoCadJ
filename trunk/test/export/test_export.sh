3b7df8691adde29e9d4dafb09be34b8b70bab7c5
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2012-04-15 09:08:54 +0000

    Improved the test suite.

--- trunk/test/export/test_export.sh
+++ trunk/test/export/test_export.sh
@@ -7,39 +7,38 @@ echo "Test script for graphical export facility of FidoCadJ"
 echo "-----------------------------------------------------"
 echo ""
 
-echo "Testing the export on bitmap formats"
-echo "PNG (Portable Network Graphic)"
-java -jar ../../jar/fidocadj.jar -n -c r2 png png/test_out.png original/test_pattern.fcd
-java -jar ../../jar/fidocadj.jar -n -c 800 600 png png/test_out_r.png original/test_pattern.fcd
-echo "JPG (Joint Photographic Experts Group)" 
-java -jar ../../jar/fidocadj.jar -n -c r2 jpg jpg/test_out.jpg original/test_pattern.fcd
-java -jar ../../jar/fidocadj.jar -n -c 800 600 jpg jpg/test_out_r.jpg original/test_pattern.fcd
+echo "Export to bitmap formats"
+echo "  PNG (Portable Network Graphic)"
+java -jar ../../jar/fidocadj.jar -n -c r2 png png/test_out.png original/test_pattern.fcd >output_png_r.txt
+java -jar ../../jar/fidocadj.jar -n -c 800 600 png png/test_out_r.png original/test_pattern.fcd >output_png.txt
+echo "  JPG (Joint Photographic Experts Group)" 
+java -jar ../../jar/fidocadj.jar -n -c r2 jpg jpg/test_out.jpg original/test_pattern.fcd >output_jpg_r.txt
+java -jar ../../jar/fidocadj.jar -n -c 800 600 jpg jpg/test_out_r.jpg original/test_pattern.fcd >output_jpg.txt
 
 echo
 
-echo Testing the export on vector formats
-echo "SVG (Scalar Vector Graphic)"
-java -jar ../../jar/fidocadj.jar -n -c r2 svg svg/test_out.svg original/test_pattern.fcd
-java -jar ../../jar/fidocadj.jar -n -c 800 600  svg svg/test_out_r.svg original/test_pattern.fcd
-java -jar ../../jar/fidocadj.jar -n -c 800 600  svg svg/led_circuit_r.svg original/led_circuit.fcd
+echo Export to vector formats
+echo "  SVG (Scalar Vector Graphic)"
+java -jar ../../jar/fidocadj.jar -n -c r2 svg svg/test_out.svg original/test_pattern.fcd >output_svg_r.txt
+java -jar ../../jar/fidocadj.jar -n -c 800 600  svg svg/test_out_r.svg original/test_pattern.fcd >output_svg_1.txt
+java -jar ../../jar/fidocadj.jar -n -c 800 600  svg svg/led_circuit_r.svg original/led_circuit.fcd >output_svg_2.txt
 
-echo "EPS (Encapsulated Postscript)"
-java -jar ../../jar/fidocadj.jar -n -c r2 eps eps/test_out.eps original/test_pattern.fcd
-echo "PDF (Portable Document Format)"
-java -jar ../../jar/fidocadj.jar -n -c r2 pdf pdf/test_out.pdf original/test_pattern.fcd
-echo "SCR (script for CadSoft Eagle)"
-java -jar ../../jar/fidocadj.jar -n -c r2 scr scr/test_out.scr original/test_pattern.fcd
-echo "PGF (PGF/TikZ script for LaTeX)"
-java -jar ../../jar/fidocadj.jar -n -c r1.5 pgf pgf/test_out.pgf original/test_pattern.fcd
+echo "  EPS (Encapsulated Postscript)"
+java -jar ../../jar/fidocadj.jar -n -c r2 eps eps/test_out.eps original/test_pattern.fcd >output_eps.txt
+echo "  PDF (Portable Document Format)"
+java -jar ../../jar/fidocadj.jar -n -c r2 pdf pdf/test_out.pdf original/test_pattern.fcd >output_pdf.txt
+echo "  SCR (script for CadSoft Eagle)"
+java -jar ../../jar/fidocadj.jar -n -c r2 scr scr/test_out.scr original/test_pattern.fcd >output_scr.txt
+echo "  PGF (PGF/TikZ script for LaTeX)"
+java -jar ../../jar/fidocadj.jar -n -c r1.5 pgf pgf/test_out.pgf original/test_pattern.fcd >output_pgf.txt
 cd pgf
-pdflatex test_out.tex
+pdflatex test_out.tex >output_latex.tex
 rm test_out.aux
 rm test_out.log
 cd ..
 echo "LaTeX test file for PGF export compiled. Please note that it is perfectly normal that the PGF export does not include the text size and font attributes, since the idea is that the user might control those aspects via LaTeX commands."
-
+echo
 echo "The reference drawing has been exported in all the supported formats. Checking the results begins now..."
 
 ./check.sh
-echo ""
-
+exit $?
--- trunk/test/messages/test_messages.sh
+++ trunk/test/messages/test_messages.sh
@@ -9,9 +9,15 @@ cd ../../bin
 reference=MessagesBundle_it.properties
 
 echo "The reference file will be " $reference
+test_failed=0
 
 for language in $(ls *.properties)
 do
-	echo $language
 	../test/messages/iscomplete.sh $language $reference
+	if test $? != 0
+	then
+		test_failed=1
+	fi
 done
+
+exit $test_failed
