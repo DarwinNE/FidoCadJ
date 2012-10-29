ea1faac82cc0badb04b0b026e8e164b6612fbfd6
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2012-10-29 21:52:01 +0000

    Updated the en and the it manuals.

--- trunk/src/globals/Globals.java
+++ trunk/src/globals/Globals.java
@@ -89,7 +89,7 @@ public class Globals
     
   								
     // Version. This is shown in the main window title bar
-    public static final String version = "0.24.1 zeta";
+    public static final String version = "0.24.1 eta";
     // Is it a beta version?
     public static final boolean isBeta = true;		
     
