3a3d138658a3687e84d71529cb40996497a1629a
darwinne <darwinne@880ea2b6-760b-4809-816f-37be44e75eeb>
2013-03-24 11:32:38 +0000

    Improving the undo system for libraries. Still some work to do.

--- trunk/src/globals/Globals.java
+++ trunk/src/globals/Globals.java
@@ -369,4 +369,37 @@ public class Globals
     	
     	return s.toString();
     }
+    
+    /**
+    http://subversivebytes.wordpress.com/2012/11/05/java-copy-directory-recursive-delete/
+    */
+    public static void copyDirectory(File sourceLocation, File targetLocation) 
+    	throws IOException 
+    {
+    	if(sourceLocation.isDirectory()) {
+        	if(!targetLocation.exists()) {
+            	targetLocation.mkdir();
+        	}
+ 
+        	String[] children = sourceLocation.list();
+        	for(int i = 0; i < children.length; i++) {
+            	copyDirectory(new File(sourceLocation, children[i]), new File(targetLocation, children[i]));
+        	}
+    	} else {
+        	InputStream in = new FileInputStream(sourceLocation);
+        	OutputStream out = new FileOutputStream(targetLocation);
+         
+       		try {
+            	byte[] buf = new byte[1024];
+            	int len;
+            	while((len = in.read(buf)) > 0) {
+                	out.write(buf, 0, len);
+            	}
+        	}
+        	finally {
+            	in.close();
+            	out.close();
+        	}
+    	}
+	}
 }
