301b22d17dc40f7463f25a8a340012dcd233b84f
Davide Bucci <DarwinNE@users.noreply.github.com>
2014-03-16 21:03:11 +0000

    Android: added the generated resources to the SVN, added a ContextMenu with some editing actions.

--- OSes/android/test/src/fr/davbucci/test/test_start.java
+++ OSes/android/test/src/fr/davbucci/test/test_start.java
@@ -4,15 +4,16 @@ import dialogs.DialogAbout;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.view.ContextMenu.*;
+
 
 import toolbars.*;
 
 public class test_start extends Activity
 {
 	private ToolbarTools tt;
-	private FidoEditor drawingpanel;
+	private FidoEditor drawingPanel;
 	private FragmentManager fragmentManager = getFragmentManager();
     /** Called when the activity is first created. */
     @Override
@@ -21,9 +22,9 @@ public class test_start extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         tt = new ToolbarTools();
-        drawingpanel = (FidoEditor)findViewById(R.id.drawingpanel);
+        drawingPanel = (FidoEditor)findViewById(R.id.drawingPanel);
         
-        tt.activateListeners(this, drawingpanel.eea);
+        tt.activateListeners(this, drawingPanel.eea);
         
     }
     
@@ -68,5 +69,61 @@ public class test_start extends Activity
 		dialog.show(fragmentManager, "");
 	    return true;
 	}	
-       
+	
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, 
+     	ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+    	MenuInflater inflater = getMenuInflater();
+   		inflater.inflate(R.menu.popupmenu, menu);
+   	
+    }
+    
+    /** Called when the user selects something in the context menu.
+    */
+   	@Override
+   	public boolean onContextItemSelected (MenuItem item)
+   	{
+   		boolean status=false;
+		
+		// Get the action selected by the user and execute it.
+		switch (item.getItemId()) 
+		{
+			case R.id.menu_param: 
+				break;			
+			case R.id.menu_cut:
+				break;
+			case R.id.menu_copy: 
+				break;			
+ 			case R.id.menu_paste: 
+ 				break;			
+			case R.id.menu_selectall:
+				drawingPanel.getEditorActions().setSelectionAll(true);
+				status=true;
+				break;
+			case R.id.menu_delete:
+				drawingPanel.getEditorActions().deleteAllSelected(true);
+				status=true;
+				break;
+			case R.id.menu_rotate:
+				drawingPanel.getEditorActions().rotateAllSelected();
+				status=true;
+				break;
+			case R.id.menu_mirror:
+				drawingPanel.getEditorActions().mirrorAllSelected();
+				status=true;
+				break;
+			default: 					
+				break;
+		}
+		
+		// If something has changed, force a redraw.
+		if (status)
+			drawingPanel.invalidate();
+			
+		return status;
+	}
+   
 }
