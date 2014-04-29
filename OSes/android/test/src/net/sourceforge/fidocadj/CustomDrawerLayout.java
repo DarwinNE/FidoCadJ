6ecc1f917cd2f5e0566815ef1d2a2b62b2af5e49
Davide Bucci <DarwinNE@users.noreply.github.com>
2014-04-29 11:16:46 +0000

    Android: Added some files I forgot and resolved a conflict on main.xml layout file.

---
+++ OSes/android/test/src/net/sourceforge/fidocadj/CustomDrawerLayout.java
@@ -0,0 +1,31 @@
+package net.sourceforge.fidocadj;
+
+import android.support.v4.widget.DrawerLayout;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View.MeasureSpec;
+
+public class CustomDrawerLayout extends DrawerLayout {
+
+    public CustomDrawerLayout(Context context) {
+        super(context);
+    }
+
+    public CustomDrawerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        widthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.EXACTLY);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
