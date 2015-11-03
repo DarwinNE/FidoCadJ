51e6142d8b1d15b7e1faae33f4000b925cce973c
Mighty Thor <mygthythor@gmail.com>
2015-11-03 20:53:24 +0100

    structure and first test

---
+++ multilanguage/languages/english/header.php
@@ -0,0 +1,16 @@
+<?php
+//this file contains the definitions of header components in English
+
+//subtitle under the FidoCadJ main log
+define ("MAIN_SUBTITLE","a free graphical editor for (more than) electronics");
+
+//top navigation menu
+define ("HOME_BUTTON","Download");
+define ("DOWNLOAD_BUTTON","Download");
+define ("SCREENSHOTS_BUTTON","Screenshots");
+define ("LIBRARIES_BUTTON","Libraries");
+define ("FAQ_BUTTON","FAQ");
+define ("EXAMPLES_BUTTON","Examples");
+
+
+ ?>
---
+++ multilanguage/languages/italian/index.php
@@ -0,0 +1,8 @@
+<?php
+//this file contains the definitions of header components in Italian
+//questo file ospita le definizioni dei testi dell'intestazione (header) in Italiano
+
+define('META_DESCRIPTION',''); //page meta tag description
+define('META_DESCRIPTION','');
+
+ ?>
