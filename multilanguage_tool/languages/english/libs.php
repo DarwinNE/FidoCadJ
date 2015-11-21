6ebb759a6a3f693e89c593003a45169b18ea1cda
Mighty Thor <mygthythor@gmail.com>
2015-11-21 06:42:58 +0100

    lips.html italian translation

---
+++ multilanguage_tool/languages/english/libs.php
@@ -0,0 +1,50 @@
+<?php
+//this file contains the definitions of text included in libs.html page
+
+//HEAD
+define("META_DESCRIPTION","FidoCadJ libreries and symbols collection is also for free. With this user-contributed content you can expand the software capabilities."); //page meta tag description
+
+define("PAGE_TITLE","FidoCadJ Libraries and Symbols"); //page title attribute
+
+//CONTENT
+define("FIRST_LINE","Libraries and symbols"); //FIRST LINE OF TEXT ON THE CONTENT SECTION OF PAGE
+
+define("PARAGRAPH_1",'You can <b>freely download</b> user-contributed libraries of symbols from here. To install them in FidoCadJ, you should choose a directory in your computer, un-compress the downloaded file there. You will find one or several files with the fcl (FidoCadJ library) extension. In FidoCadJ, go in the program settings and in the "startup" tab select the directory containing the files with the fcl extension.'); //PARAGRAPH TEXT
+
+define("LIBRARY_1",'By elettrodomus, from <a href="http://www.electroyou.it/elettrodomus/wiki/una-piccola-libreria-ladder-per-fidocadj">this EY article</a>, ladder logic symbols (in italian, license CC BY-NC 4.0).'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_2",'By elettrodomus, from <a href="http://www.electroyou.it/forum/viewtopic.php?f=4&t=54536&start=250#p560385">this EY discussion</a>, musical symbols (in italian, license CC BY-NC 4.0).'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_3",'By mir, from <a href="http://www.electroyou.it/mir/wiki/libreria-pneumatica-per-fidocadj">this EY article</a>, a nice collection of symbols for <b>pneumatic circuits</b> (in italian, license CC BY-NC 4.0).'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_4",'By Franco Minucci, a very nice collection of <b>macro-circuits</b> and different symbols.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_5",'Proposed by Paolo Squaratti, this library contains symbols for ECMA-4 <b>flowcharts</b>, from <a href="http://www.electroyou.it/pepito/wiki/libreria-flowchart-per-fidocadj">ElectroYou</a>.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_6",'Proposed by Alberto Pellegrini, a collection of <b>hydraulic</b> installation symbols.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_7",'Proposed by <a href="http://www.picexperience.it/">Paolo Rognoni</a> in this article: <a href="http://www.electroyou.it/mediawiki/index.php?title=UsersPages:Paolino:lo-hai-mai-realizzato-con-un-pic-due-librerie-cad-per-pierin-pic18">LO HAI MAI REALIZZATO CON UN PIC? Due librerie CAD per PIERIN PIC18</a>, this library is released with license CREATIVE COMMONS BY-NC-ND 3.0. It allows to use very easily the <a href="http://www.sangon.it/index_en.html">PIERIN PIC18</a> development system with FidoCadJ.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_8",'Proposed by Luigi C. in <a href="http://groups.google.it/group/it.hobby.elettronica/browse_thread/thread/cbf672dff997bfe9/718122cdca661ade?hl=it&">this message on an italian Usenet group</a>. This library contains new symbols for <b>schematics</b>: binary encoders, interface and digital integrated circuits, PICs.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_9",'Proposed by Luigi C. in <a href="http://groups.google.it/group/it.hobby.elettronica/browse_thread/thread/cbf672dff997bfe9/718122cdca661ade?hl=it&">this message on an italian Usenet group</a>. This library contains new symbols for <b>schematics</b>: binary encoders, interface and digital integrated circuits, PICs.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_10",'Proposed by moderators of Usenet group it.hobby.radioamatori.moderato and downloaded from <a href="http://www.iz1cyn.it/fidocad.html">the Roby  IZ1CYN website</a> (in Italian language). This library contains many useful symbols for <b>electronic circuits and PCBs</b>. This library is now included by default in FidoCadJ.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_11",'Macro collection proposed by Tullio, Juan Bravo on it.hobby.elettronica. I assembled all the macros in a single library file which is contained in the zip archive.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_12",'Library available on <a href="http://tomasella.altervista.org/it/elettronica/pcb/#disegno_schema">this website</a> (link in Italian). It contains the <b>footprints</b> of several connectors (USB in particular) and of several SMD packages.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_13",'Library provided by Lorenzo Neri. It contains some SSOP footprints which corrects some <b>errors</b> of the standard footprints library.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_14",'By Nobody, from the it.discussioni.ingegneria.civile Usenet group. It contains some actions and stresses useful for <b>structure analysis</b>.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_15",'By Mario i3HEV, it contains several footprints of <b>RF devices</b>: transistors, inductors, transformers...'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_16",'By Maurizio Mileto, it contains a vast collection of symbols for <b>civil and industrial</b> electric installations. FidoCadJ includes by default version 2.0 of this library.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("LIBRARY_17",'Library prepared by MacZap in <a href="http://www.grix.it/forum/forum_thread.php?ftpage=1&amp;id_forum=1&amp;id_thread=401194&amp;tbackto=/forum/forum_discussioni.php?id_forum=1&amp;dbackto=/forum/index.php">this discussion</a> and dedicated to <b>Arduino</b>.'); //LIBRARY DESCRIPTION IN TABLE
+
+define("PARAGRAPH_2",'If you have prepared a library file for FidoCadJ or FidoCad and you want to share it between the user community, please, participate to the FidoCadJ forum on Sourceforge. On the other hand, if you have found here something which should <b>not</b> be here, please also write a message on the Sourceforge forum or <a href="mailto:davbucciAVOIDSPAM@tiscali.it"> contact the webmaster by email</a>, eliminating the capital letters in the address.'); //PARAGRAPH TEXT
+
+ ?>
