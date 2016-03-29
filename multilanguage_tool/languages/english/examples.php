7fa020ca15963de1a7a9713dab955de59536b1d1
Mighty Thor <mygthythor@gmail.com>
2016-03-29 06:32:16 +0200

    examples.html page translation

---
+++ multilanguage_tool/languages/english/examples.php
@@ -0,0 +1,16 @@
+<?php
+//this file contains the definitions of text included in scrn.html page
+
+//HEAD
+define("META_DESCRIPTION","Examples of FidoCadJ code and capabilities. Draw electrical stuffs from the schema to the pcb footprint. The FidoCadJ file format is easy to use and very light."); //page meta tag description
+define("PAGE_TITLE","FidoCadJ Examples"); //page title attribute
+
+//CONTENT
+define("FIRST_LINE","Copy the code and paste it in FidoCadJ"); //FIRST LINE OF TEXT ON THE CONTENT SECTION OF PAGE
+
+define("IMAGE_ALT_1","A differential amplifier"); //ALT ATTRIBUTE OF IMAGE
+
+define("IMAGE_ALT_2","Nixie clock PCB"); //ALT ATTRIBUTE OF IMAGE
+
+
+ ?>
---
+++ multilanguage_tool/languages/italian/examples.php
@@ -0,0 +1,17 @@
+<?php
+//this file contains the definitions of text included in scrn.html page
+
+//HEAD
+define("META_DESCRIPTION","Esempi delle potenzialità di FidoCadJ. Schemi elettrici dallo schema al layout del PCB. Il formato dei file di FidoCadJ è semplice da usare e molto leggero."); //page meta tag description
+
+define("PAGE_TITLE","Esempi in FidoCadJ"); //page title attribute
+
+//CONTENT
+define("FIRST_LINE","Copia e incolla questo codice in FidoCadJ"); //FIRST LINE OF TEXT ON THE CONTENT SECTION OF PAGE
+
+define("IMAGE_ALT_1","Un amplificatore differenziale"); //ALT ATTRIBUTE OF IMAGE
+
+define("IMAGE_ALT_2","PCB dell'orologio Nixie"); //ALT ATTRIBUTE OF IMAGE
+
+
+ ?>
