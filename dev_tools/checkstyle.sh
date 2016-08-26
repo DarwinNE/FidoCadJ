e9ad3553f6c7e181b6078b17b8f74e2e866fc632
DanteCpp <danteloi1993@gmail.com>
2016-08-26 16:51:47 +0200

    Fixed checkstyle warning.

--- dev_tools/checkstyle.sh
+++ dev_tools/checkstyle.sh
@@ -4,9 +4,9 @@ if [ "$1" = "" ]; then
   echo "You should specify the checkstyle jar (with path) as an argument."
   echo 'Example: ./checkstyle.sh ~/CodeAnalysis/checkstyle/checkstyle-6.10-all.jar'
   exit 1
-fi 
+fi
 
-java -jar $1 -c rules.xml src/net/sourceforge/fidocadj/**/*  src/net/sourceforge/fidocadj/* >fidocadj_check.txt
+java -jar $1 -c rules.xml ../src/net/sourceforge/fidocadj/**/*  ../src/net/sourceforge/fidocadj/* >fidocadj_check.txt
 
 echo "Checkstyle results stored in fidocadj_check.txt"
 echo "lines on fidocadj_check.txt"
