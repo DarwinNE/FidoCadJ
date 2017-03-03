64be4433162d705e09ea81ef2dc62a63992fdb9c
DarwinNE <davbucci@gmail.com>
2017-03-03 23:00:42 +0100

    Greatly improve the redraw speed with images at the background (#99) with large zoom factors.

--- src/net/sourceforge/fidocadj/circuit/ImageAsCanvas.java
+++ src/net/sourceforge/fidocadj/circuit/ImageAsCanvas.java
@@ -39,6 +39,9 @@ public class ImageAsCanvas
     private double xcorner=0;
     private double ycorner=0;
 
+    private final int MAX_RESIZED_WIDTH=3000;
+    private final int MAX_RESIZED_HEIGHT=3000;
+
     /** Constructor.
     */
     public ImageAsCanvas()
@@ -121,6 +124,8 @@ public class ImageAsCanvas
 
     private int oldw=0;
     private int oldh=0;
+    private int shiftx=0;
+    private int shifty=0;
 
     /** Draw the current image in the given graphic context.
         @param g the Graphic2D object where the image has to be drawn.
@@ -131,30 +136,89 @@ public class ImageAsCanvas
         if(img==null)
             return;
 
+        // The image is drawn only in the "dirty" region of the drawing area
+        // so to greatly improve redrawing speed.
+        Rectangle clip=g.getClipBounds();
+        int regionx=clip.x;
+        int regiony=clip.y;
+        int regionwidth=clip.width;
+        int regionheight=clip.height;
+
+
         // The FidoCadJ resolution is 200dpi.
-        int w=(int)(200*img.getWidth()/resolution*mc.getXMagnitude()+0.5);
-        int h=(int)(200*img.getHeight()/resolution*mc.getYMagnitude()+0.5);
+        int w=(int)(200.0*img.getWidth()/resolution*mc.getXMagnitude()+0.5);
+        int h=(int)(200.0*img.getHeight()/resolution*mc.getYMagnitude()+0.5);
+
+        int ox=mc.mapXi(xcorner, ycorner,false);
+        int oy=mc.mapYi(xcorner, ycorner,false);
+
+        // This code is needed to avoid exceeding the boundaries of the
+        // images (this produces a tiled effect).
+        regionwidth=Math.max(0,Math.min(regionwidth,w-regionx+ox));
+        regionheight=Math.max(0,Math.min(regionheight,h-regiony+ox));
+        regionx=Math.max(ox,regionx);
+        regiony=Math.max(oy,regiony);
 
         // Resizing an image is pretty time-consuming. Therefore, this is done
-        // only when it is absolutely needed (usually when the zoom is
-        // changed).
-        if(oldw!=w || oldh!=h) {
+        // only when it is absolutely needed. This happens when the zoom is
+        // changed, or when the chunk of the image which has been resized
+        // should be changed.
+
+        if(oldw!=w || oldh!=h || regionx<shiftx+ox || regiony<shifty+oy||
+            regionx+regionwidth>shiftx+ox+MAX_RESIZED_WIDTH ||
+            regiony+regionheight>shifty+oy+MAX_RESIZED_HEIGHT)
+        {
             GraphicsEnvironment env =
                 GraphicsEnvironment.getLocalGraphicsEnvironment();
             GraphicsDevice device = env.getDefaultScreenDevice();
             GraphicsConfiguration config = device.getDefaultConfiguration();
-            // Well, I have some doubts here. I suspect that in some case the
-            // image size would be requiring an excessive amount of memory.
-            // I will perform some test when I will have a minimum of user
-            // interface ready so that the tests become feasible.
-            resizedImg = config.createCompatibleImage(
-                w, h, Transparency.TRANSLUCENT);
-
-            resizedImg.getGraphics().drawImage(img,0,0,w,h,null);
             oldw=w;
             oldh=h;
+            if(w<MAX_RESIZED_WIDTH && h<MAX_RESIZED_HEIGHT) {
+                // Here the image can be resized all together.
+                shiftx=0;
+                shifty=0;
+                resizedImg = config.createCompatibleImage(
+                    w, h, Transparency.TRANSLUCENT);
+
+                resizedImg.getGraphics().drawImage(img,0,0,w,h,null);
+            } else {
+                // Here, resizing the image would produce an image too big.
+                // Therefore, the image is resized by chunks.
+
+                /*System.out.println("\nPartial image calculation");
+                System.out.println("shiftx="+shiftx+" shifty="+shifty);
+                System.out.println("regionx="+regionx+" regiony="+regiony);
+                System.out.println("regionx+regionwidth="+(regionx+regionwidth)+
+                    " regiony+regionheight="+(regiony+regionheight));*/
+
+                resizedImg = config.createCompatibleImage(
+                    MAX_RESIZED_WIDTH, MAX_RESIZED_HEIGHT,
+                        Transparency.TRANSLUCENT);
+                shiftx=Math.max(regionx-MAX_RESIZED_WIDTH/3,0);
+                shifty=Math.max(regiony-MAX_RESIZED_HEIGHT/3,0);
+                resizedImg.getGraphics().drawImage(img, 0, 0,
+                    MAX_RESIZED_WIDTH,
+                    MAX_RESIZED_HEIGHT,
+                    (int)(shiftx/mc.getXMagnitude()*resolution/200.0+0.5),
+                    (int)(shifty/mc.getXMagnitude()*resolution/200.0+0.5),
+                    (int)((shiftx+MAX_RESIZED_WIDTH)
+                        /mc.getXMagnitude()*resolution/200.0
+                        +0.5),
+                    (int)((shifty+MAX_RESIZED_HEIGHT)/
+                            mc.getYMagnitude()*resolution/200.0+0.5),
+                    null);
+            }
         }
-        g.drawImage(resizedImg, mc.mapXi(xcorner, ycorner,false),
-                mc.mapYi(xcorner, ycorner,false), null);
+
+        // Draw the resized image at the right place.
+        g.drawImage(resizedImg, regionx, regiony,
+            regionx+regionwidth,
+            regiony+regionheight,
+            regionx-shiftx-ox,
+            regiony-shifty-oy,
+            regionx+regionwidth-shiftx-ox,
+            regiony+regionheight-shifty-oy,
+            null);
     }
 }
