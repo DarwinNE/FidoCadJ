f8ab7c999d7fe85b1ce467d3db4a0a9fa93dfe03
DarwinNE <davbucci@gmail.com>
2017-05-01 23:51:00 +0200

    Updated website to download version 0.24.7.

--- multilanguage_tool/config.php
+++ multilanguage_tool/config.php
@@ -3,17 +3,17 @@
 //elements that do not require translation but frequently changes
 
 //FidoCadJ download links
-define ("MAC_VERSION_DOWNLOAD_LINK","https://github.com/DarwinNE/FidoCadJ/releases/download/v0.24.6/FidoCadJ_MacOSX.dmg");
+define ("MAC_VERSION_DOWNLOAD_LINK","https://github.com/DarwinNE/FidoCadJ/releases/download/v0.24.7/FidoCadJ_MacOSX.dmg");
 define ("WINDOWS_VERSION_DOWNLOAD_LINK","https://github.com/DarwinNE/FidoCadJ/releases/download/v0.24.6/FidoCadJ_0.24.6_Windows.msi");
-define ("LINUX_VERSION_DOWNLOAD_LINK","https://github.com/DarwinNE/FidoCadJ/releases/download/v0.24.6/fidocadj.jar");
+define ("LINUX_VERSION_DOWNLOAD_LINK","https://github.com/DarwinNE/FidoCadJ/releases/download/v0.24.7/fidocadj.jar");
 define ("ANDROID_VERSION_DOWNLOAD_LINK","http://sourceforge.net/projects/fidocadj/files/public_betas/Android/fidocadj-debug20150227.apk/download");
 
 //FidoCadJ current version (which is ready to distribute)
-define ("CURRENT_VERSION","0.24.6");
+define ("CURRENT_VERSION","0.24.7");
 
-define ("CURRENT_MAC_VERSION","0.24.6");
+define ("CURRENT_MAC_VERSION","0.24.7");
 define ("CURRENT_WINDOWS_VERSION","0.24.6");
-define ("CURRENT_LINUX_VERSION","0.24.6");
+define ("CURRENT_LINUX_VERSION","0.24.7");
 define ("CURRENT_ANDROID_VERSION","0.24.5 kappa");
 
 
@@ -32,12 +32,12 @@ define ("JAVA_VERSION_REQUIRED_OLD_VERSION", "1.5");
 
 //last editing per page (will appear at the end of each page)
 //USE FORMAT MM/DD/YY
-define ("INDEX_PAGE_UPDATE_DATE", "7.04.2016");
-define ("DOWNLOAD_PAGE_UPDATE_DATE", "11.04.2016");
-define ("SCREEN_SHOTS_PAGE_UPDATE_DATE", "7.04.2016");
-define ("LIBRARIES_PAGE_UPDATE_DATE", "7.04.2016");
-define ("FAQ_PAGE_UPDATE_DATE", "7.04.2016");
-define ("EXAMPLES_PAGE_UPDATE_DATE", "7.04.2016");
+define ("INDEX_PAGE_UPDATE_DATE", "1.05.2017");
+define ("DOWNLOAD_PAGE_UPDATE_DATE", "1.05.2017");
+define ("SCREEN_SHOTS_PAGE_UPDATE_DATE", "1.05.2017");
+define ("LIBRARIES_PAGE_UPDATE_DATE", "1.05.2017");
+define ("FAQ_PAGE_UPDATE_DATE", "1.05.2017");
+define ("EXAMPLES_PAGE_UPDATE_DATE", "1.05.2017");
 
 
  ?>
